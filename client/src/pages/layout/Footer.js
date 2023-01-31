0ab185b8b99ceb6ee2dfc1a3673cc0df6232cafd
sarang <sarang.office01@gmail.com>
2023-01-31 16:07:49 +0530

    pages and routes added

---
+++ client/src/pages/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{background:'#3a4750',textAlign:'center',color:'white',padding:'25px'}}>
+        <h4 >
+            All Right Reserver &copy; Sarang
+        </h4>
+    </div>
+  )
+}
+
+export default Footer
