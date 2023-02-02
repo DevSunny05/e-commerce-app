9e54b15e99e20825ffff4a6138ec24faa2ea402f
sarang <sarang.office01@gmail.com>
2023-02-02 09:43:44 +0530

    SEO added in the pages

--- client/src/pages/layout/Layout.js
+++ client/src/pages/layout/Layout.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import {Helmet} from 'react-helmet'
 
-const Layout = ({ children }) => {
+const Layout = ({ children,title,description,keywords,author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords}/>
+            <meta name="author" content={author} />
+        
+
+      </Helmet>
+
       <Header />
       <main style={{minHeight:'77vh' }}>
         {children}
@@ -13,5 +25,12 @@ const Layout = ({ children }) => {
     </div>
   );
 };
+
+Layout.defaultProps={
+  title:'E-commerce',
+  description:'mern stack project',
+  keywords:'mern,react,node,express,css',
+  author:'sarang'
+}
 
 export default Layout;
